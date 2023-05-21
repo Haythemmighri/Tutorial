cefd42e85e91571ca763aad2670c86f9405446f4
Haythem Mighri <122802240+Haythemmighri@users.noreply.github.com>
2023-05-21 15:26:18 +0100

    Add files via upload

---
+++ certificat full stack Web developper/first.js
@@ -0,0 +1,96 @@
+console.log("connected with externel file");
+
+//  var a =1;
+// var b =2;
+// if(a+b>4){
+//  console.log(a+b);
+    
+// }
+// else if(a+b<1){
+// console.log(a+b);
+// }
+// else{
+//    console.log("yes it's 3");
+// }
+
+// for (var num=0; num<11;num=num+2){
+// console.log(num);
+// }
+
+// var a = [1,2,3,4];
+// a.forEach(item => console.log(item));
+
+//  for(var item in a){
+//     console.log(item);
+// }
+// var i=0;
+// while(i<5){
+//    console.log(i);
+//     i++;
+// }
+
+// do{
+//  console.log(i);
+//  i++
+// }while(i<5);
+
+// var itr = 10;
+// console.log(itr++);//10
+// console.log(itr);//11
+
+// console.log(++itr);//12
+// console.log(itr);//12
+
+// var num1=8,n=3;
+// console.log(num1<<n);
+// console.log(num1>>n);
+
+// console.log(a);
+// var a=10;
+// console.log(a);
+
+// let weather="sunny";
+// function changeWeather(){
+//     let weather="rainy";
+//     console.log("changed weather:"+weather)
+// }
+// changeWeather();
+// console.log("original weather:"+weather);
+
+// for(var n=0;n<5;n++)
+//     console.log(n);
+
+// console.log(n);    
+
+// var name="haythem";
+// console.log(name+"is 21 yrs old");
+// console.log(`${name} is 21 yrs old`);
+
+// var obj = {
+//     name:'haythem',
+//     roll_no:10,
+//     sing:function(){
+//         console.log(`${this.name} sings`)
+//     }
+// }
+// console.log(obj.name);
+// obj.sing();
+// console.log(obj["roll_no"]);
+
+var a = [1,2,3,4,5,6,7,8,9];
+a.push(2);
+
+sg();
+
+function sg(){
+    console.log("DSVSV");
+
+}
+var name = "James";
+var roll_no = 30;
+console.log( name + roll_no );
+var a = -10.2256;
+console.log( typeof(a));
+
+var a = 10;
+console.log( typeof(typeof(a)));
